4287e9e6c105d5273949d45a331af67fc5a07f2d
Isabel Restrepo <isabelrestre@gmail.com>
2021-04-08 15:36:55 -0400

    refactor(wip): use nuxt-content on dynamic index

--- pages/_main/index.vue
+++ pages/_main/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- General template for routes (About, Services...)  -->
   <div>
     <DHero
       variant="primary"
@@ -6,6 +7,7 @@
       :subtitle="index.description"
       class="hero"
     >
+      <!-- Add a button to the Hero when index.yml includes call for action -->
       <template v-if="index['call-for-action']" #button>
         <nuxt-link
           class="d-button is-dark has-text-light"
@@ -16,45 +18,55 @@
         </nuxt-link>
       </template>
     </DHero>
-    <ListTemplate v-if="$route.params.main === 'help'" :data="data" />
-    <About v-else-if="$route.params.main === 'about'" :data="data" :toc="toc" />
-    <ListTemplate v-else :data="list.map((d) => d.index)" />
+    <!-- <ListTemplate v-if="$route.params.main === 'help'" :data="data" /> -->
+    <About v-if="$route.params.main === 'about'" :data="data" />
+    <!-- <ListTemplate v-else :data="list.map((d) => d.index)" /> -->
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
 import { DHero } from '@brown-ccv/disco-vue-components';
-import ListTemplate from '@/components/blocks/ListTemplate.vue';
-import About from '@/components/blocks/About.vue';
+// import ListTemplate from '@/components/blocks/ListTemplate.vue';
+import About from '@/components/blocks/About2.vue';
 
 export default {
   components: {
     DHero,
-    ListTemplate,
     About
   },
-  async fetch({ store, params, error }) {
-    await store.dispatch('content/fetchData', params);
-  },
-  computed: mapState({
-    data: (state) => state.content.data,
-    index: (state) => state.content.index,
-    toc: (state) => state.content.toc,
-    list: (state) => state.content.list
-  }),
-  head() {
+  async asyncData({ $content, params }) {
+    const index = await $content(`${params.main}/index`).fetch();
+    const data = await $content(`${params.main}`, params.slug)
+      .where({ title: { $ne: 'About' } })
+      .sortBy('title', 'desc')
+      .fetch();
     return {
-      title: this.index.title, // gets appended to templateTitle from default layout title
-      meta: [
-        {
-          hid: 'description', // important to override template description tag
-          name: 'description',
-          content: this.index.description
-        }
-      ]
+      index,
+      data
     };
   }
+  //   async fetch({ store, params, error }) {
+  //     await store.dispatch('content/fetchData', params);
+  //   },
+  //   computed: mapState({
+  //     data: (state) => state.content.data,
+  //     index: (state) => state.content.index,
+  //     toc: (state) => state.content.toc,
+  //     list: (state) => state.content.list
+  //   })
+  //   This is the status banner
+  //   head() {
+  //     return {
+  //       title: this.index.title, // gets appended to templateTitle from default layout title
+  //       meta: [
+  //         {
+  //           hid: 'description', // important to override template description tag
+  //           name: 'description',
+  //           content: this.index.description
+  //         }
+  //       ]
+  //     };
+  //   }
 };
 </script>
 
