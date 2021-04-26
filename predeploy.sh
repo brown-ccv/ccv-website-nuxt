6332056b0d0f049c5369f4e1bf926c665027cc72
Mary McGrath <m.c.mcgrath13@gmail.com>
2021-04-26 17:21:53 -0400

    ci: fix predeploy, clean up post deploy

--- predeploy.sh
+++ predeploy.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 # clean up old state
-rm -rf dist/*
+rm -rf dist
 rm -rf functions/content
 rm -rf functions/.nuxt/*\ncp -r .nuxt functions
 
 # set up new state
-cp -r content functions/content
+mkdir dist
+cp -r content functions
 cp nuxt.config.js functions
 cp -r .nuxt/dist/client dist
