3b92bd5e41d439ecaa460449ccdac5de169d038d
ashleysyg <ashley.syg.lee@gmail.com>
2023-08-21 09:33:50 -0400

    feat: detail card imitates main card

--- components/blocks/OurWorkDetail.vue
+++ components/blocks/OurWorkDetail.vue
@@ -3,19 +3,54 @@
     <div class="mt-5">
       <DCard class="mx-3 my-3 px-3" variant="light" accent="light" width="full">
         <template #header>
-          <!-- <div class="is-flex is-justify-content-center">
-            <figure class="image detail-image-bio">
-              <img :src="'/content/images/people/' + detail" />
-            </figure>
-          </div> -->
-          <div class="media-content">
-            <h2 class="title has-text-black pt-6">{{ detail }}</h2>
-            <h2 class="subtitle has-text-black">{{ detail }}</h2>
-            <h2 class="subtitle has-text-black">{{ detail }} | {{ detail }}</h2>
+          <template v-for="(tagClass, tagType) in tagColors">
+            <span
+              v-for="tag in data[tagType]"
+              :key="tag"
+              class="radius-0 tag m-1"
+              :class="tagClass"
+            >
+              {{ tag }}
+            </span>
+          </template>
+          <h2 class="title has-text-black pt-3">{{ data.title }}</h2>
+          <div v-if="data.date">Updated: {{ data.date }}</div>
+          <span>
+            <div
+              v-for="(contributorArray, contributorType) in contributors(data)"
+              :key="contributorType"
+            >
+              <div>
+                <i
+                  v-if="contributorArray"
+                  :class="['mdi', contributorIcon[contributorType], 'mdi-24px']"
+                ></i>
+              </div>
+              <span
+                v-for="(entry, index) in contributorArray"
+                :key="entry.name"
+              >
+                <a :href="contributorLink(entry)">{{ entry.name }}</a
+                ><span v-if="index + 1 < contributorArray.length">, </span>
+              </span>
+            </div>
+          </span>
+          <div>
+            <p>{{ data.description }}</p>
           </div>
         </template>
         <template #footer>
-          <div class="content">{{ data }}</div>
+          <section v-if="data.links" class="link-group">
+            <div><i class="mdi mdi-link p-1 title"></i></div>
+            <a
+              v-for="data in data.links"
+              :key="data.url"
+              class="m-1 link-item d-button has-background-link has-text-white has-text-weight-semibold is-size-5 link-button"
+              :href="data.url"
+            >
+              <span>{{ data.category.toUpperCase() }} </span>
+            </a>
+          </section>
         </template>
       </DCard>
     </div>
@@ -34,9 +69,46 @@ export default {
       required: true,
     },
   },
-  async asyncData({ $content, params }) {
-    const detail = await $content('our-work').fetch();
-    return { detail };
+  data: () => ({
+    ascending: true,
+    sortBy: [],
+    searchGroup: [],
+    tags: ['department', 'groups', 'tags', 'languages', 'active'],
+    tagColors: {
+      department: 'is-yellow',
+      groups: 'is-yellow',
+      tags: 'is-link',
+      languages: 'is-info',
+      active: 'is-info',
+    },
+    contributorIcon: {
+      investigators: 'mdi-brain',
+      people: 'mdi-account-multiple',
+    },
+  }),
+  methods: {
+    clearAll() {
+      this.searchGroup = [];
+    },
+    contributors(card) {
+      const subset = (({ investigators, people }) => ({
+        investigators,
+        people,
+      }))(card);
+      return subset;
+    },
+    contributorLink(contributor) {
+      if ('link' in contributor) {
+        return contributor.link;
+      } else if ('github_user' in contributor) {
+        return 'https://github.com/' + contributor.github_user;
+      } else {
+        return undefined;
+      }
+    },
+    common(a, b) {
+      return b.filter(Set.prototype.has.bind(new Set(a)));
+    },
   },
 };
 </script>
