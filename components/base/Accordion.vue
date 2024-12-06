cdeb965997497e4843c0a3fab7145a55899928aa
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-12-06 13:49:47 -0500

    feat: accordion component

---
+++ components/base/Accordion.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="column is-half">
+    <div class="card" :class="{ 'not-expanded': !this.expanded }">
+      <header class="card-header" @click="toggleCardState">
+        <p class="card-header-title">
+          {{title}}
+        </p>
+        <a class="card-header-icon">
+            <span class="icon">
+              <i class="fa fa-angle-up"></i>
+            </span>
+        </a>
+      </header>
+
+      <div class="card-content">
+        <div class="content">
+          <slot></slot>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    expandAll: Boolean
+  },
+  data() {
+    return {
+      expanded: false,
+    };
+  },
+  methods: {
+    toggleCardState() {
+      this.expanded = !this.expanded;
+    },
+    closeAll() {
+      this.expanded = false;
+    }
+  },
+
+};
+</script>
